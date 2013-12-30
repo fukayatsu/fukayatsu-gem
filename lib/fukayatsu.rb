ab5a1c32506248665144445e11a3bcbeee35d499
fukayatsu <fukayatsu@gmail.com>
2013-12-31 00:59:34 +0900

    [from now] 2013/12/31 00:59:34

--- lib/fukayatsu.rb
+++ lib/fukayatsu.rb
@@ -6,5 +6,13 @@ module Fukayatsu
       @name = 'fukayatsu'
     end
     attr_reader :name
+
+    def man?; true; end
+    def love_ruby?; true; end
+    def love_coding?; true; end
+    def love_sushi?; true; end
+    def email; 'fukayatsu@gmail.com'; end
+    def blog; 'http://blog.fukayatsu.com/'; end
+    def has_girlfriend?; !!!!!!!!!!!!!!!!!!!!!!!!!!!"＼(^o^)／"; end
   end
 end
--- spec/fukayatsu_spec.rb
+++ spec/fukayatsu_spec.rb
@@ -3,4 +3,11 @@ require 'spec_helper'
 describe Fukayatsu::Core do
   subject { described_class.new }
   it { expect(subject.name).to eq('fukayatsu') }
+  it { expect(subject.man?).to be true }
+  it { expect(subject.love_ruby?).to be true }
+  it { expect(subject.love_coding?).to be true }
+  it { expect(subject.love_sushi?).to be true }
+  it { expect(subject.email).to eq('fukayatsu@gmail.com') }
+  it { expect(subject.blog).to eq('http://blog.fukayatsu.com/')}
+  it { expect(subject.has_girlfriend?).to be true}
 end
